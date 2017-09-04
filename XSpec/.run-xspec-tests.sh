f6e4df150379feecb56557ee96774def149847ae
Tom Hillman <yamahito@gmail.com>
2017-09-04 16:27:28 +0100

    Updated travis config

--- XSpec/.run-xspec-tests.sh
+++ XSpec/.run-xspec-tests.sh
@@ -1,7 +1,12 @@
 for xspectest in *.xspec; 
-    do /tmp/xspec/bin/xspec.sh $xspectest &> result.log; 
-    if grep -q ".*failed:\s[1-9]" result.log || grep -q "*\sError\srunning\sthe\stest\ssuite" result.log; 
-	    then echo "$xspectest failed" && exit 1; 
-	    else echo "ok $xspectest";
-    fi	
+do /tmp/xspec/bin/xspec.sh $xspectest &> result.log; 
+    if grep -q ".*failed:\s[1-9]" result.log || grep -q -E "\*+\sError\s(running|compiling)\sthe\stest\ssuite" result.log;
+        then
+            echo "FAILED: $xspectest";
+            echo "---------- result.log";
+            cat result.log;
+            echo "----------";
+            exit 1;
+        else echo "OK: $xspectest";
+    fi
 done
